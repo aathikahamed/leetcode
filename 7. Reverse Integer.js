c0c6aef97c81c216812bb63675e07f942e896c0e
aathik <aathikahamed@gmail.com>
2021-05-17 15:07:05 +0100

    completed problem number 7

---
+++ 7. Reverse Integer.js
@@ -0,0 +1,55 @@
+/***
+
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+Input: x = 123
+Output: 321
+
+Example 2:
+Input: x = -123
+Output: -321
+
+Example 3:
+Input: x = 120
+Output: 21
+
+Example 4:
+Input: x = 0
+Output: 0
+
+Constraints:
+-2^31 <= x <= 2^31 - 1
+
+***/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+var reverse = function (x) {
+  let numlist = x.toString().split("");
+  let isNegative = false;
+  if (numlist[0].startsWith("-")) {
+    isNegative = true;
+    numlist = numlist.splice(1, numlist.length);
+  }
+
+  numlist = numlist.reverse();
+
+  let reverseNum = numlist.join("");
+
+  reverseNum = `${isNegative ? "-" : ""}${reverseNum}`;
+
+  reverseNum = parseInt(reverseNum);
+
+  if (reverseNum < -(2 ** 31) || reverseNum > 2 ** 31 - 1) {
+    return 0;
+  }
+  return reverseNum;
+};
+
+console.log(reverse(-123));
